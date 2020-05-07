9d517b423e350ab66cd55904185fb65be9067f4d
Yeaseen <1405091.ya@ugrad.cse.buet.ac.bd>
2020-05-07 20:48:02 +0600

    added necessary functions

--- ArrayList.cpp
+++ ArrayList.cpp
@@ -4,15 +4,38 @@
 int main(){
 
     ArrayList myList;
+
     myList.add(12);
     myList.add(12.345);
     myList.add(12.11f);
     myList.add("Its high time ...");
+    myList.add(53);
+
+    cout << "The capacity of the list is: " << myList.getCapacity() << endl;
     cout << "The size of the list is: " << myList.getSize() << endl;
-    myList.printAvalue(0);
-    myList.printAvalue(1);
+    myList.printALL();
+
+    cout<<endl;
+
     myList.printAvalue(2);
-    myList.printAvalue(3);
+
+    cout<<endl;
+
+    myList.removeAtId(0);
+
+    cout << "The capacity of the list is: " << myList.getCapacity() << endl;
+    cout << "The size of the list is: " << myList.getSize() << endl;
+    myList.printALL();
+
+    if(myList.move(0,2)) cout<<"Move is done successfully";
+    else cout<<"Move is not applicable";
+
+    myList.printALL();
+
+    myList.reverseArray();
+
+    myList.printALL();
+
 
 
 }
--- ArrayList.h
+++ ArrayList.h
@@ -16,21 +16,31 @@ private:
     int arraySize;
     int currentSize;
     MultiType *data = nullptr;
+    void reallocate();
 
 public:
-    ArrayList();
+     ArrayList();
     ~ArrayList();
+
 
      void add(const MultiType & element);
      const MultiType * gets(int index) const;
      int getSize() const;
-     void printAvalue(int id);
+     int getCapacity() const;
+     void removeAtId(int id);
+     void removeAll(int newCapacity);
+
+     bool move(int index1, int index2);
+     void reverseArray();
+     void printAvalue(int id) const;
+     void printALL() const;
+
 
 };
 
 
 ArrayList::ArrayList() {
-    arraySize = 10;
+    arraySize = 2;
     currentSize = 0;
     data = new MultiType[arraySize];
 
@@ -42,8 +52,29 @@ ArrayList :: ~ArrayList() {
 
 
 
+void ArrayList::reallocate() {
+
+    arraySize *= 2;
+    MultiType * temp = new MultiType[arraySize];
+
+    for (int i = 0; i < currentSize; i++) {
+
+        temp[i] = data[i];
+    }
+
+    delete [] data;
+
+    data = temp;
+
+}
+
+
 void ArrayList::add(const MultiType & element) {
 
+     if ((currentSize ) == arraySize) {
+
+        reallocate();
+    }
 
     data[currentSize] = element;
     currentSize++;
@@ -57,33 +88,93 @@ const MultiType * ArrayList::gets(int index) const {
     return (index >= 0 && index < currentSize) ? (data + index) : NULL;
 }
 
+void ArrayList::removeAtId(int id) {
+
+    if (id < currentSize && id >= 0) {
+
+        for (int i = id; i < currentSize; i++) {
+
+            data[i] = data[i + 1];
+        }
+        currentSize--;
+        arraySize--;
+    }
+
+}
+
+void ArrayList::removeAll(int newCapacity) {
+
+    delete [] data;
+    data = new MultiType[newCapacity];
+
+    arraySize = newCapacity;
+    currentSize = 0;
+}
+
+
+bool ArrayList::move(int index1, int index2) {
+
+    if (index1 >= 0 && index2 >= 0 &&
+            index1 < currentSize && index2 < currentSize) {
+
+        MultiType temp = data[index1];
+        data[index1] = data[index2];
+        data[index2] = temp;
+
+        return true;
+    }
+
+    return false;
+}
+
+void ArrayList::reverseArray()
+{
+	for (int low = 0, high = currentSize - 1; low < high; low++, high--) {
+		move(low, high);
+	}
+}
+
 
 int ArrayList::getSize() const {
 
     return currentSize;
 }
 
+int ArrayList::getCapacity() const {
 
-void ArrayList::printAvalue(int id){
+    return arraySize;
+}
+
+
+void ArrayList::printAvalue(int id) const{
 
     if(!(id >= 0 && id < currentSize)){
         cout << "Sorry! You are not permitted at this location"<<endl;
         return;
     }
     MultiType p = *gets(id);
+
     if(holds_alternative<int>(p)) {
-      cout << get<int>(p) << '\n';
+      cout<<"Data Type: int ------  Data value: " << get<int>(p) << '\n';
     }
     else if(holds_alternative<string>(p)){
-        cout << get<string>(p) << '\n';
+        cout<<"Data Type: string ------  Data value: " << get<string>(p) << '\n';
     }
     else if(holds_alternative<float>(p)){
-        cout << get<float>(p) << '\n';
+        cout<<"Data Type: float ------  Data value: " << get<float>(p) << '\n';
     }
     else if(holds_alternative<double>(p)){
-        cout << get<double>(p) << '\n';
+        cout<<"Data Type: double ------  Data value: " << get<double>(p) << '\n';
     }
 
+}
+
+void ArrayList::printALL() const{
+
+    cout << endl;
+    for(int i=0; i<currentSize; i++){
+        printAvalue(i);
+    }
 }
 
 #endif /* ARRAYLIST_H_ */
